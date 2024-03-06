6fb74ae812a15d4ccc9071dceeb318f3e2d476bd
lopppes <sl263920@gmail.com>
2024-03-06 14:56:43 -0300

    test imagem

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/vista-studios/"
+  base: "/vista-studios/",
+  plugins: [react()]
 })
